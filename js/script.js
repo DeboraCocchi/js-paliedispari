3773932f950169cf480580ab3fe55c50cf620fe1
DeboraCocchi <cccdbr@gmail.com>
2022-10-19 18:45:11 +0200

    [end]js-side

--- js/script.js
+++ js/script.js
@@ -12,18 +12,20 @@ Dichiariamo chi ha vinto.*/
 
 
 /* da chiedere:
-1-perchè non corrisponde l'output; 2- perchè non ha preso il pattern dell'input*/
+1- perchè non ha preso il pattern dell'input*/
 
 const userInput = document.getElementById('user-word');
 const oputputField = document.querySelector('.verifica-parola');
 const wordBtn = document.getElementById('sub-word');
 
 wordBtn.addEventListener('click', function(){
-  checkWord(userInput.value);
+  const verificaPalindroma = checkWord(userInput.value);
   oputputField.innerHTML += verificaPalindroma;
+  userInput.value = '';
+  oputputField.innerHTML +='';
 });
 
-const verificaPalindroma = checkWord('userInput.value');
+
 
 
 function checkWord(parola){
@@ -31,22 +33,22 @@ function checkWord(parola){
   for (let i=0; i<parola.length; i++){
       if(parola[i] === parola[parola.length-1-i]){
         console.log('la parola è palindroma!');
+        console.log(parola);
         return 'è palindroma';
-      }return 'NON è palindroma, prova con un\'altra parola';
-      
+        
+      }else{
+        console.log('la parola NON è palindroma!');
+        console.log(parola);
+        return 'NON è palindroma, prova con un\'altra parola';}
+   
   }
+  
 };
+userInput.value='';
 
 // pari e dispari
 
-const computerRisult = computerNumber();
-console.log(computerRisult);
 
-function computerNumber(){
-  const pcResult = Math.ceil(Math.random()*(5));
-  return pcResult;
-
-}
 
 const userNumber = document.getElementById('user-number');
 const userEvenOdd = document.getElementById('user-evenodd');
@@ -56,11 +58,52 @@ const sumOutput = document.querySelector('.verifica-somma');
 
 userChoice.addEventListener('click', function(){
   console.log(userNumber.value, userEvenOdd.value);
-  if(userNumber.value<1 ||userNumber.value>5 || isNaN(userNumber.value) || userEvenOdd.value !=='pari' ||userEvenOdd.value !=='dispari'){
-    sumOutput.innerHTML = 'Inserisci tutti i dati come richiesto';
-  }sumOutput.innerHTML = 'bravo';
-  
+
+  //verifico che l'utente inserisca i dati come richiesto
+  if((userNumber.value>=1 && userNumber.value<=5)&&(userEvenOdd.value==='pari' || userEvenOdd.value==='dispari')){
+
+    //ha compilato correttamente entrambi i campi
+    sumOutput.innerHTML = 'I dati sono validi';
+
+    //lancio la funzione del numero rdm del pc, salvando il risultato in una const
+    const pcNumber = computerNumber();
+    console.log('al pc è uscito il numero ' + pcNumber);
+
+    //recupero il numero inserito dallo user e lancio la funzione pari e dispari
+    const ourSum = evenOddNumbersSum(parseInt(userNumber.value),pcNumber);
+    console.log('la somma è ' + ourSum);
+
+    if(userEvenOdd.value===ourSum){
+      console.log(userEvenOdd.value , ourSum);
+      sumOutput.innerHTML = `Hai indovinato il risultato! La somma dei due numeri è <strong>${ourSum}</strong>, come avevi previsto!`;
+    }else{sumOutput.innerHTML = `Per questa volta non hai indovinato il risultato, ma puoi sempre riprovare!`;}
+
+  //qui verifico la correttezza dei campi compilati e impedisco alla funzione di andare avanti
+  }else if(userNumber.value<1 ||userNumber.value>5 || isNaN(userNumber.value) || userEvenOdd.value !=='pari' ||userEvenOdd.value !=='dispari' || userEvenOdd.value=='' ){
+    sumOutput.innerHTML = 'Inserisci tutti i dati correttamente, come richiesto';
+  }
+
+  userEvenOdd.value ='';
+  userNumber.value = '';
 });
+
+
+
+function evenOddNumbersSum(num1,num2){
+  const somma = num1+num2;
+  if(somma % 2){
+    console.log('La somma dei due numeri è ' + somma +', dispari');
+    return 'dispari';
+  } 
+  console.log('La somma dei due numeri è ' + somma +', pari');
+  return 'pari';
+}
+
+
+function computerNumber(){
+  pcResult = Math.ceil(Math.random()*(5));
+  return pcResult;
+}
 
 
 
